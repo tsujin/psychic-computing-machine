4886851659b565b8a391de1b0cd482edb018db44
Justin <contact@tsujin.com>
2022-06-07 00:15:59 -0400

    ++ wavefunction class

--- src/procgen/wavefunctioncollapse.py
+++ src/procgen/wavefunctioncollapse.py
@@ -1,14 +1,11 @@
+import math
+
 DIRS = {
     'N': (0, -1),
     'S': (0, 1),
     'E': (1, 0),
     'W': (-1, 0)
 }
-
-test_map = [
-    ['G', 'G'],
-    ['W', 'M']
-]
 
 def neighboring_directions(coordinate: tuple[int, int], map_width: int, map_height: int) -> list[str]:
     """
@@ -36,8 +33,7 @@ def neighboring_directions(coordinate: tuple[int, int], map_width: int, map_heig
     return neighbors
 
 
-
-def parse_tile_map(tile_map: list[list[str]]) -> tuple[set, dict]:
+def parse_tile_map(tile_map: list[list[str]]) -> tuple[set[str], dict[str, int]]:
     """
     Parses a tilemap and returns possible neighbor tile types and weights for each tile
         
@@ -64,3 +60,41 @@ def parse_tile_map(tile_map: list[list[str]]) -> tuple[set, dict]:
                 possible_neighbors.add((tile, tile_map[i_y + dir_y][i_x + dir_x], dir))
     
     return possible_neighbors, weights_map
+
+
+class WaveFunctionEngine:
+    """Class for collapsing the wave function and building our map"""
+    def __init__(self, neighbors_map: set[str], weights: dict):
+        self.neighbors_map = neighbors_map
+        self.weights = weights
+
+    def compute_shannon_entropy(self, square1: tuple, square2: tuple):
+        #log(sum(weight)) -
+        #(sum(weight * log(weight)) / sum(weight))
+        pass
+
+    def set_initial_coefficients(self, map_size: tuple[int, int], tiles: list[str]) -> list[list[str]]:
+        """
+        Function which sets the initial state of the map, where every point has all tiles as possible options
+        Args:
+            map_size (tuple[int, int]): Size of the NxN map
+
+            tiles (list[str]): List of all possible tile types
+        
+        Returns:
+            A 2-dimensional list containing each possible tile for each point
+        """
+
+        width, height = map_size
+        new_map = []
+
+        for x in range(width):
+            for y in range(height):
+                new_row = []
+                new_row.append(tiles)
+            new_map.append(new_row)
+        
+        return new_map
+
+    def collapse_tile(self):
+        pass
